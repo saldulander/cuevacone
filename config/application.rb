5aacf0e572559f6c5f47202260dcc53a8b82f03b
Saldulander <tomysaldutto@hotmail.com>
2020-11-19 14:29:25 -0300

    First theme configuration, adding helpers and some assets

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module Cuevacone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+#agrego esta linea para evitar el error de Zeitwerk
+    config.autoloader = :classic
+
+
+
   end
 end
